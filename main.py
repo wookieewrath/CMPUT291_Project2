2d342b9d79e97b7478716630dbabf30febcb2239
bsamimi1 <39813078+bsamimi1@users.noreply.github.com>
2019-11-22 16:15:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import xml.etree.ElementTree as ET
+
 def main():
     print(" Hello World ^_^ ")
 
+    tree = ET.parse('10.xml')
+    root = tree.getroot()
+    count = len(tree.findall('mail'))
+
+    date_file = open("dates.txt","w+")
+                
+    for x in root.findall('mail'):
+        date = x.find('date').text
+        row_id = x.find('row').text
+        body = x.find('body').text
+        date_file.write("%s: %s\n" %(date,row_id))
+                           
+date_file.close()
 
 if __name__ == "__main__":
     main()
