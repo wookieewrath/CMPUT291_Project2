f7b459f03d4c78acd34a0c3c96558cf4b1ac521d
wookieewrath <44443467+wookieewrath@users.noreply.github.com>
2019-11-22 16:32:23 -0700

    Fixed indentation issues

--- main.py
+++ main.py
@@ -1,52 +1,52 @@
 import xml.etree.ElementTree as ET
 
-def main():
 
-#Load the input XML file into Python3 as an Element Tree.
-#Create a tree and root object to navigate the file, and a count object to dictate the summation of elements.
+def main():
+    # Load the input XML file into Python3 as an Element Tree.
+    # Create a tree and root object to navigate the file, and a count object to dictate the summation of elements.
     file = input("Please print the name of the XML file")
     tree = ET.parse(file)
     root = tree.getroot()
     count = len(tree.findall('mail'))
 
-#Creates terms.txt
-length = len(tree.findall("mail"))
-alphabet = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_0123456789')
-terms = open("terms.txt", "w+")
-for x in range(length-1):
-    if root[x][4].text is not None:
-        string = root[x][4].text.split()
-        for y in string:
-            if (len(''.join(filter(alphabet.__contains__, y)).lower()) > 2):
-                terms.write("s-" + ''.join(filter(alphabet.__contains__, y)).lower() + ":" + root[x][0].text + "\n")
-    if root[x][7].text is not None:
-        string = root[x][7].text.split()
-        for y in string:
-            if(len(''.join(filter(alphabet.__contains__, y)).lower())>2):
-                terms.write("b-" + ''.join(filter(alphabet.__contains__, y)).lower() + ":" + root[x][0].text + "\n")
 
-#Creates the "dates.txt" file from the XML input using the following format:
-#"date":"row_id"
-    date_file = open("dates.txt","w+")      
+    # Creates terms.txt
+    length = len(tree.findall("mail"))
+    alphabet = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_0123456789')
+    terms = open("terms.txt", "w+")
+    for x in range(length - 1):
+        if root[x][4].text is not None:
+            string = root[x][4].text.split()
+            for y in string:
+                if (len(''.join(filter(alphabet.__contains__, y)).lower()) > 2):
+                    terms.write("s-" + ''.join(filter(alphabet.__contains__, y)).lower() + ":" + root[x][0].text + "\n")
+        if root[x][7].text is not None:
+            string = root[x][7].text.split()
+            for y in string:
+                if (len(''.join(filter(alphabet.__contains__, y)).lower()) > 2):
+                    terms.write("b-" + ''.join(filter(alphabet.__contains__, y)).lower() + ":" + root[x][0].text + "\n")
+
+    # Creates the "dates.txt" file from the XML input using the following format:
+    # "date":"row_id"
+    date_file = open("dates.txt", "w+")
     for x in root.findall('mail'):
         date = x.find('date').text
         row_id = x.find('row').text
         body = x.find('body').text
-        date_file.write("%s: %s\n" %(date,row_id))          
+        date_file.write("%s: %s\n" % (date, row_id))
     date_file.close()
-    
-#Creates the "emails.txt" file from the XML input using the following format:
-#from-"from_address":"row_id"
-#to-"to_address":"row_id"    
-emails_file = open("emails.txt", "w+")
-for x in root.findall('mail'):
-    from_address = x.find('from').text
-    to_address = x.find('to').text
-    row_id = x.find('row').text
-    emails_file.write("from-%s:%s\n" % (from_address, row_id))
-    emails_file.write("to-%s:%s\n" % (to_address, row_id))
-emails_file.close()
+
+    # Creates the "emails.txt" file from the XML input using the following format:
+    # from-"from_address":"row_id"
+    # to-"to_address":"row_id"    
+    emails_file = open("emails.txt", "w+")
+    for x in root.findall('mail'):
+        from_address = x.find('from').text
+        to_address = x.find('to').text
+        row_id = x.find('row').text
+        emails_file.write("from-%s:%s\n" % (from_address, row_id))
+        emails_file.write("to-%s:%s\n" % (to_address, row_id))
+    emails_file.close()
 
 if __name__ == "__main__":
     main()
-    
