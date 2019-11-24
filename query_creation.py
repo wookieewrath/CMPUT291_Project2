472cc39e164fbfb82379c8664c3f27969d4ecab2
kamillahhasham <37892254+kamillahhasham@users.noreply.github.com>
2019-11-24 01:57:15 -0700

    Update query_creation.py
    Commented the code

--- query_creation.py
+++ query_creation.py
@@ -7,7 +7,7 @@ decomposed_string = list(search_string) #Will decompose the original search_stri
 query = {} #Initialize the query dictionary
 
 symbols = ["<",">",":"]
-keywords = ["row","date", "from", "to", "subject", "cc", "bcc", "body"]
+keywords = ["row", "date", "from", "to", "subject", "subj", "cc", "bcc", "body"]
 
 #While we are not at the end of the list, continue to loop through the decomposed_string until you reach a symbol.
 #Insert white space before and after the symbol.
@@ -24,15 +24,20 @@ while True:
         else:
                 item = item+1
 
+#Rejoin the decomposed_string into a string with white spaces.
+#The purpose of this was to ensure that every symbol in the query has white space around itself to isolate it when we call .split()
 string = "".join(decomposed_string)
 
+#string.split() will remove all white spaces and create an array of search fields, symbols, and search values.
 search_terms = string.split()
 
+#Search this array for symbols.
+#If the search_term before the symbol is a keyword, create a dictionary entry.
 for i in range(len(search_terms)):
         if search_terms[i] in symbols:
                 if search_terms[i-1] in keywords:
                         query[search_terms[i-1]] = search_terms[i] + search_terms[i+1]
-
+                        
 print(query)
 
 
