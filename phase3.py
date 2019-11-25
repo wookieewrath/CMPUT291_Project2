b728513857e86523ffb083b862f19fb589b73250
wookieewrath <44443467+wookieewrath@users.noreply.github.com>
2019-11-25 12:58:07 -0700

    Update phase3.py

--- phase3.py
+++ phase3.py
@@ -353,6 +353,7 @@ def main():
             for row in some_list:
                 result = recs_curs.set(row)
                 print(result[1].decode("utf-8"))
+                print("\n")
 
         elif mode == "brief":
             print("")
