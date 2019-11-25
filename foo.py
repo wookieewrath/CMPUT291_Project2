8d3d4f42959e3dd0f1e94c9a39e8e05a38ab4b6a
Neel <wookieewrath@gmail.com>
2019-11-25 09:25:00 -0700

    next_lex

--- foo.py
+++ foo.py
@@ -2,15 +2,17 @@
 
 
 
+def foo(mystring):
+
+    for x in range(len(mystring)-1, -1, -1):
+        if mystring[x] == "z":
+            mystring = mystring[:-1]
+            return foo(mystring)
+        else:
+            last = mystring[-1]
+            mystring = mystring[:-1] + chr(ord(last)+1)
+            return mystring
 
 
-
-recs = open('backslash.txt', "r")
-
-for line in recs:
-
-    line = line.replace("\\", "")
-    print(line)
-
-recs.close()
-
+x = foo("abcdefgzzzz")
+print(x)
