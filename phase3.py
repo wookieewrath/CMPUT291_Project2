fdfbeaf2832f98d60ffa584cade98d077b5cb65d
bsamimi1 <39813078+bsamimi1@users.noreply.github.com>
2019-11-24 21:05:14 -0700

    Update phase3.py

--- phase3.py
+++ phase3.py
@@ -3,13 +3,107 @@ from query_creation import query_creation
 
 
 def terms_search(curs, term):
-    pass
+    result = curs.set(bytes(term,'utf-8'))
+    query_set = set()
+    
+    while True:
+        if result is not None:
+            query_set.add(result[1])
+            result = curs.next_dup()
+        else:
+            break
+    
+    return query_set    
+    
+    
 
 def dates_search(curs, term):
-    pass
+    symbol = ''
+    length = len(term)
+    query_set = set()
+    i = 0
+    for i in range(3):
+        if term[i].isdigit() == False:
+            symbol += term[i]
+        
+        else:
+            j=i
+            break
+    term = term[j:]
+    print(term)
+    print(symbol)
+
+    result = curs.set(bytes(term,'utf-8'))
+    
+    if symbol == ':':
+        while True:
+            if result is not None:
+                query_set.add(result[1])
+                result = curs.next_dup()
+            else:
+                break
+    
+    elif symbol == '>':
+        result = curs.next_nodup()
+        while True:
+            if result is not None:
+                query_set.add(result[1])
+                result = curs.next()
+            else:
+                break        
+        
+    
+    elif symbol == '<':
+        result = curs.prev()
+        while True:
+            if result is not None:
+                query_set.add(result[1])
+                result = curs.prev()
+            
+            else:
+                break
+                
+    elif symbol == '>=':
+        while True:
+            if result is not None:
+                query_set.add(result[1])
+                result = curs.next()
+            else:
+                break         
+        
+    
+    elif symbol == '<=':
+        while True:
+            if result is not None:
+                query_set.add(result[1])
+                result = curs.next_dup()
+            else:
+                break
+            
+        result = curs.prev_nodup()
+        while True:
+            if result is not None:
+                query_set.add(result[1])
+                result = curs.prev()
+            else:
+                break    
+    
+    return query_set
 
 def emails_search(curs, term):
-    pass
+    
+    result = curs.set(bytes(term,'utf-8'))
+    query_set = set()
+    
+    while True:
+        if result is not None:
+            query_set.add(result[1])
+            result = curs.next_dup()
+        else:
+            break
+    
+    return query_set
+                      
 
 
 def main():
@@ -40,23 +134,29 @@ def main():
     
     while True:
         foo = input("Please enter a search query: ")
+        i = 0
+        
+        if foo == 'stop':
+            break
         
         search_dict = query_creation(foo)
-        
-        
+        print("This is the search dictionary")
+        print(search_dict)
         
         for database in search_dict:
             if database == "terms_curs":
                 for term in search_dict[database]:
                     terms_search(terms_curs, term)
+
+                    
                     
             if database == "dates_curs":
                 for term in search_dict[database]:
-                    dates_search(dates_curs, term)
+                    print(dates_search(dates_curs, term))
                     
             if database == "emails_curs":
                 for term in search_dict[database]:
-                    emails_search(emails_curs, term)
+                    print(emails_search(emails_curs, term))
                     
             if database == "subj_or_body":
                 pass               
